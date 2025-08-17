a6a2d774923bd466bb0ad722bce4f00b0ca45f39
bishoy19 <97695425+bishoy19@users.noreply.github.com>
2025-08-17 21:06:43 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
-};
-};
+}
 
 export default nextConfig;
